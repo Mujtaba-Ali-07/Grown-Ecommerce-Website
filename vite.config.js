24197ec7a87f13710f34f37ec7d0d07328e934e2
Mujtaba-Ali-07 <mujtabaalee92@gmail.com>
2025-06-12 14:36:14 +0500

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "Grown-Ecommerce-Website",
+  base: "/Grown-Ecommerce-Website/",
   plugins: [react(), tailwindcss()],
 });
